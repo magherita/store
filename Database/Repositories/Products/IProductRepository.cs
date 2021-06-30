19cd7f5918adc4b08e15487e87bd0620035f1b78
LAPTOP-53MB4V4Q\Walter_Ruganzu <walter@laboremus.no>
2021-06-30 10:20:34 +0300

    First Commit

--- Database/Repositories/Products/IProductRepository.cs
+++ Database/Repositories/Products/IProductRepository.cs
@@ -8,6 +8,16 @@ namespace Database.Repositories.Products
 {
     public interface IProductRepository
     {
+        // Define Product CRUD methods.
+        Task CreateProductAsync(Product product, CancellationToken cancellationToken = default);
 
+        Task<Product> RetrieveProductAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<List<Product>> RetrieveProductAsync(CancellationToken cancellationToken = default);
+
+        void UpdateProductAsync(Product product);
+
+        void DeleteProductAsync(Product product);
+    
     }
 }
--- Database/Repositories/Products/ProductRepository.cs
+++ Database/Repositories/Products/ProductRepository.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace Database.Repositories.Products
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
 
+        private readonly StoreContext _context;
+
+        public ProductRepository (StoreContext context)
+	    {
+            this._context = context; 
+	    }
+
+        public async Task CreateProductAsync(Product  product, CancellationToken cancellationToken = default)
+        {
+           await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+
+        public Task DeleteProduct(Product product, CancellationToken cancellationToken = default)
+        {
+            _context.Remove(product);
+
+
+        }
+
+        public Task<Product> RetrieveProductAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> RetrieveProductAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
